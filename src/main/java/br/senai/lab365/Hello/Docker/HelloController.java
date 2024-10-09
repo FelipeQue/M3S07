2094e1d54fa654cfa224605c558e4989f66297f6
Felipe Quérette <felipe.querette@gmail.com>
2024-10-08 22:04:56 -0300

    fix: HelloController

--- src/main/java/br/senai/lab365/Hello/Docker/HelloController.java
+++ src/main/java/br/senai/lab365/Hello/Docker/HelloController.java
@@ -1,14 +1,14 @@
 package br.senai.lab365.Hello.Docker;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RequiredArgsConstructor
+@RestController
 @RequestMapping("/hello")
 public class HelloController {
 
